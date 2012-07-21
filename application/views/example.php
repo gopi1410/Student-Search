1eea3d1d758b2f50773ebbfb6de54a450d43968b
Gopi Ramena <gopiramena@gmail.com>
2012-07-21 20:49:19 +0530

    initial commit

--- application/views/example.php
+++
@@ -1,8 +0,0 @@
-<h1><a href='<?= $logout_url ?>'>Logout</a></h1>
-
-<?php foreach ($api_info['data'] as $user) { ?>
-    <?php foreach ($user as $item) {?>
-
-        <?= $item ?><br />
-
-<?php } } ?>
